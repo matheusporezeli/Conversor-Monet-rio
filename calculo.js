7f1b2a4fc4a51095bb3bfa38165b2f214269f1d5
Matheus Porezeli <112051389+matheusporezeli@users.noreply.github.com>
2022-10-28 12:03:28 -0300

    Atualização dos valores - 28/10

--- calculo.js
+++ calculo.js
@@ -8,52 +8,52 @@ function Converter() {
       if (moeda2 === "real") {
         resultado.innerHTML = "R$ " + valorOriginal;
       } else if (moeda2 === "dolar") {
-        var resultadoD = valorOriginal * 0.19633;
+        var resultadoD = valorOriginal * 0.1887;
         resultado.innerHTML = "$ " + resultadoD.toFixed(2);
       } else if (moeda2 === "euro") {
-        var resultadoE = valorOriginal * 0.19391;
+        var resultadoE = valorOriginal * 0.189;
         resultado.innerHTML = "€ " + resultadoE.toFixed(2);
       } else if (moeda2 === "peso") {
-        var resultadoP = valorOriginal * 27.92707;
+        var resultadoP = valorOriginal *  29.3772;
         resultado.innerHTML = "ARS $ " + resultadoP.toFixed(2);
       }
     } else if (moeda1 === "dolar") {
       if (moeda2 === "dolar") {
         resultado.innerHTML = "$ " + valorOriginal;
       } else if (moeda2 === "real") {
-        var resultadoR = valorOriginal * 5.0935;
+        var resultadoR = valorOriginal * 5.2997;
         resultado.innerHTML = "R$ " + resultadoR.toFixed(2);
       } else if (moeda2 === "euro") {
-        var resultadoE = valorOriginal * 0.98762;
+        var resultadoE = valorOriginal * 1.0015;
         resultado.innerHTML = "€ " + resultadoE.toFixed(2);
       } else if (moeda2 === "peso") {
-        var resultadoP = valorOriginal * 142.2898;
+        var resultadoP = valorOriginal * 155.69;
         resultado.innerHTML = "ARS $ " + resultadoP.toFixed(2);
       }
     } else if (moeda1 === "euro") {
       if (moeda2 === "euro") {
         resultado.innerHTML = "€ " + valorOriginal;
       } else if (moeda2 === "real") {
-        var resultadoR = valorOriginal * 5.1562;
+        var resultadoR = valorOriginal * 5.2907;
         resultado.innerHTML = "R$ " + resultadoR.toFixed(2);
       } else if (moeda2 === "dolar") {
-        var resultadoD = valorOriginal * 1.0123;
+        var resultadoD = valorOriginal * 0.9983;
         resultado.innerHTML = "$ " + resultadoD.toFixed(2);
       } else if (moeda2 === "peso") {
-        var resultadoP = valorOriginal * 144.0036;
+        var resultadoP = valorOriginal * 155.4253;
         resultado.innerHTML = "ARS $ " + resultadoP.toFixed(2);
       }
     } else if (moeda1 === "peso") {
       if (moeda2 === "peso") {
         resultado.innerHTML = valorOriginal;
       } else if (moeda2 === "real") {
-        var resultadoR = valorOriginal * 0.03579;
+        var resultadoR = valorOriginal * 0.03404 ;
         resultado.innerHTML = "R$ " + resultadoR.toFixed(2);
       } else if (moeda2 === "euro") {
-        var resultadoE = valorOriginal * 0.006944;
+        var resultadoE = valorOriginal * 0.006434;
         resultado.innerHTML = "€ " + resultadoE.toFixed(2);
       } else if (moeda2 === "dolar") {
-        var resultadoD = valorOriginal * 0.007028;
+        var resultadoD = valorOriginal * 0.0064222;
         resultado.innerHTML = "$ " + resultadoD.toFixed(2);
       }
     }
